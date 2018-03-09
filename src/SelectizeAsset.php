3962f89332aac0e9a3e5af56d6fc8bdee4716422
Choate Yao <choate@localhost.localdomain>
2018-03-09 18:48:09 +0800

    init

---
+++ src/SelectizeAsset.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Choate
+ * Date: 2018/3/9
+ * Time: 18:42
+ */
+
+namespace choate\yii2\selectize;
+
+
+use yii\web\AssetBundle;
+
+class SelectizeAsset extends AssetBundle
+{
+
+}
